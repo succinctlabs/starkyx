96bfc81eccaee0d968034012b0eede919a7e494b
Tamir Hemo <tamir@succinct.xyz>
2023-08-04 11:42:12 -0700

    new shr

--- curta/src/chip/uint/bytes/bit_operations/mod.rs
+++ curta/src/chip/uint/bytes/bit_operations/mod.rs
@@ -1,5 +1,4 @@
 use self::and::And;
-use self::shr::Shr;
 use self::xor::Xor;
 use crate::air::parser::AirParser;
 use crate::air::AirConstraint;
@@ -17,93 +16,93 @@ pub mod shl;
 pub mod shr;
 pub mod xor;
 
-#[derive(Debug, Clone, Copy)]
-pub enum BitOperation<const NUM_BITS: usize, const LOG_N: usize> {
-    And(And<NUM_BITS>),
-    Xor(Xor<NUM_BITS>),
-    Shr(Shr<NUM_BITS, LOG_N>),
-}
+// #[derive(Debug, Clone, Copy)]
+// pub enum BitOperation<const NUM_BITS: usize> {
+//     And(And<NUM_BITS>),
+//     Xor(Xor<NUM_BITS>),
+//     Shr(ArrayRegister<BitRegister>, ArrayRegister<BitRegister>, ArrayRegister<BitRegister>),
+// }
 
-impl<const NUM_BITS: usize, const LOG_N: usize> BitOperation<NUM_BITS, LOG_N> {
-    pub fn a_bits(&self) -> ArrayRegister<BitRegister> {
-        match self {
-            BitOperation::And(and) => and.a,
-            BitOperation::Xor(xor) => xor.a,
-            BitOperation::Shr(shr) => shr.a,
-        }
-    }
+// impl<const NUM_BITS: usize> BitOperation<NUM_BITS> {
+//     pub fn a_bits(&self) -> ArrayRegister<BitRegister> {
+//         match self {
+//             BitOperation::And(and) => and.a,
+//             BitOperation::Xor(xor) => xor.a,
+//             BitOperation::Shr(shr) => shr.a,
+//         }
+//     }
 
-    pub fn b_bits(&self) -> ArrayRegister<BitRegister> {
-        match self {
-            BitOperation::And(and) => and.b,
-            BitOperation::Xor(xor) => xor.b,
-            BitOperation::Shr(shr) => shr.b,
-        }
-    }
+//     pub fn b_bits(&self) -> ArrayRegister<BitRegister> {
+//         match self {
+//             BitOperation::And(and) => and.b,
+//             BitOperation::Xor(xor) => xor.b,
+//             BitOperation::Shr(shr) => shr.b,
+//         }
+//     }
 
-    pub fn result_bits(&self) -> ArrayRegister<BitRegister> {
-        match self {
-            BitOperation::And(and) => and.result,
-            BitOperation::Xor(xor) => xor.result,
-            BitOperation::Shr(shr) => shr.results[LOG_N - 1],
-        }
-    }
-}
+//     pub fn result_bits(&self) -> ArrayRegister<BitRegister> {
+//         match self {
+//             BitOperation::And(and) => and.result,
+//             BitOperation::Xor(xor) => xor.result,
+//             BitOperation::Shr(shr) => shr.results[LOG_N - 1],
+//         }
+//     }
+// }
 
-impl<AP: AirParser, const NUM_BITS: usize, const LOG_N: usize> AirConstraint<AP>
-    for BitOperation<NUM_BITS, LOG_N>
-{
-    fn eval(&self, parser: &mut AP) {
-        match self {
-            BitOperation::And(and) => and.eval(parser),
-            BitOperation::Xor(xor) => xor.eval(parser),
-            BitOperation::Shr(shr) => shr.eval(parser),
-        }
-    }
-}
+// impl<AP: AirParser, const NUM_BITS: usize> AirConstraint<AP>
+//     for BitOperation<NUM_BITS>
+// {
+//     fn eval(&self, parser: &mut AP) {
+//         match self {
+//             BitOperation::And(and) => and.eval(parser),
+//             BitOperation::Xor(xor) => xor.eval(parser),
+//             BitOperation::Shr(shr) => shr.eval(parser),
+//         }
+//     }
+// }
 
-impl<F: Field, const NUM_BITS: usize, const LOG_N: usize> Instruction<F>
-    for BitOperation<NUM_BITS, LOG_N>
-{
-    fn inputs(&self) -> Vec<MemorySlice> {
-        match self {
-            BitOperation::And(and) => Instruction::<F>::inputs(and),
-            BitOperation::Xor(xor) => Instruction::<F>::inputs(xor),
-            BitOperation::Shr(shr) => Instruction::<F>::inputs(shr),
-        }
-    }
+// impl<F: Field, const NUM_BITS: usize, const LOG_N: usize> Instruction<F>
+//     for BitOperation<NUM_BITS>
+// {
+//     fn inputs(&self) -> Vec<MemorySlice> {
+//         match self {
+//             BitOperation::And(and) => Instruction::<F>::inputs(and),
+//             BitOperation::Xor(xor) => Instruction::<F>::inputs(xor),
+//             BitOperation::Shr(shr) => Instruction::<F>::inputs(shr),
+//         }
+//     }
 
-    fn trace_layout(&self) -> Vec<MemorySlice> {
-        match self {
-            BitOperation::And(and) => Instruction::<F>::trace_layout(and),
-            BitOperation::Xor(xor) => Instruction::<F>::trace_layout(xor),
-            BitOperation::Shr(shr) => Instruction::<F>::trace_layout(shr),
-        }
-    }
+//     fn trace_layout(&self) -> Vec<MemorySlice> {
+//         match self {
+//             BitOperation::And(and) => Instruction::<F>::trace_layout(and),
+//             BitOperation::Xor(xor) => Instruction::<F>::trace_layout(xor),
+//             BitOperation::Shr(shr) => Instruction::<F>::trace_layout(shr),
+//         }
+//     }
 
-    fn write(&self, writer: &TraceWriter<F>, row_index: usize) {
-        match self {
-            BitOperation::And(and) => Instruction::<F>::write(and, writer, row_index),
-            BitOperation::Xor(xor) => Instruction::<F>::write(xor, writer, row_index),
-            BitOperation::Shr(shr) => Instruction::<F>::write(shr, writer, row_index),
-        }
-    }
-}
+//     fn write(&self, writer: &TraceWriter<F>, row_index: usize) {
+//         match self {
+//             BitOperation::And(and) => Instruction::<F>::write(and, writer, row_index),
+//             BitOperation::Xor(xor) => Instruction::<F>::write(xor, writer, row_index),
+//             BitOperation::Shr(shr) => Instruction::<F>::write(shr, writer, row_index),
+//         }
+//     }
+// }
 
-impl<const N: usize, const LOG_N: usize> From<And<N>> for BitOperation<N, LOG_N> {
-    fn from(and: And<N>) -> Self {
-        BitOperation::And(and)
-    }
-}
+// impl<const N: usize, const LOG_N: usize> From<And<N>> for BitOperation<N, LOG_N> {
+//     fn from(and: And<N>) -> Self {
+//         BitOperation::And(and)
+//     }
+// }
 
-impl<const N: usize, const LOG_N: usize> From<Xor<N>> for BitOperation<N, LOG_N> {
-    fn from(xor: Xor<N>) -> Self {
-        BitOperation::Xor(xor)
-    }
-}
+// impl<const N: usize, const LOG_N: usize> From<Xor<N>> for BitOperation<N, LOG_N> {
+//     fn from(xor: Xor<N>) -> Self {
+//         BitOperation::Xor(xor)
+//     }
+// }
 
-impl<const N: usize, const LOG_N: usize> From<Shr<N, LOG_N>> for BitOperation<N, LOG_N> {
-    fn from(shr: Shr<N, LOG_N>) -> Self {
-        BitOperation::Shr(shr)
-    }
-}
+// impl<const N: usize, const LOG_N: usize> From<Shr<N, LOG_N>> for BitOperation<N, LOG_N> {
+//     fn from(shr: Shr<N, LOG_N>) -> Self {
+//         BitOperation::Shr(shr)
+//     }
+// }
--- curta/src/chip/uint/bytes/bit_operations/shr.rs
+++ curta/src/chip/uint/bytes/bit_operations/shr.rs
@@ -4,102 +4,72 @@
 //! a << (b + c) = (a << b) << c
 //! a << (b + 2^i c) = (a << b) << 2^i c = ((a << b) << c) << 2^(i-1) c
 
-use itertools::Itertools;
-
-use crate::air::parser::AirParser;
-use crate::air::AirConstraint;
-use crate::chip::instruction::Instruction;
+use crate::chip::bool::SelectInstruction;
+use crate::chip::builder::AirBuilder;
 use crate::chip::register::array::ArrayRegister;
 use crate::chip::register::bit::BitRegister;
-use crate::chip::register::memory::MemorySlice;
-use crate::chip::register::RegisterSerializable;
-use crate::chip::trace::writer::TraceWriter;
+use crate::chip::AirParameters;
 pub use crate::math::prelude::*;
 
-#[derive(Debug, Clone, Copy)]
-pub struct Shr<const NUM_BITS: usize, const LOG_N: usize> {
-    pub a: ArrayRegister<BitRegister>,
-    pub b: ArrayRegister<BitRegister>,
-    pub results: [ArrayRegister<BitRegister>; LOG_N],
-}
+impl<L: AirParameters> AirBuilder<L> {
+    pub fn shr(
+        &mut self,
+        a: &ArrayRegister<BitRegister>,
+        b: &ArrayRegister<BitRegister>,
+    ) -> ArrayRegister<BitRegister>
+    where
+        L::Instruction: From<SelectInstruction<BitRegister>>,
+    {
+        let result = self.alloc_array::<BitRegister>(a.len());
+        self.set_shr(a, b, &result);
+        result
+    }
 
-impl<AP: AirParser, const NUM_BITS: usize, const LOG_N: usize> AirConstraint<AP>
-    for Shr<NUM_BITS, LOG_N>
-{
-    fn eval(&self, parser: &mut AP) {
-        debug_assert_eq!(self.a.len(), NUM_BITS);
-        debug_assert_eq!(self.b.len(), NUM_BITS);
-        let a = self.a.eval_array::<_, NUM_BITS>(parser);
-        let b = self.b.eval_array::<_, LOG_N>(parser);
-        let results = self
-            .results
-            .iter()
-            .map(|r| r.eval_array::<_, NUM_BITS>(parser))
-            .collect::<Vec<_>>();
+    pub fn set_shr(
+        &mut self,
+        a: &ArrayRegister<BitRegister>,
+        b: &ArrayRegister<BitRegister>,
+        result: &ArrayRegister<BitRegister>,
+    ) where
+        L::Instruction: From<SelectInstruction<BitRegister>>,
+    {
+        let n = a.len();
+        let m = b.len();
+        assert!(m <= n, "b must be shorter or eual length to a");
 
-        let mut temp = a;
-        let one = parser.one();
-        for (k, (bit, result)) in b.into_iter().zip_eq(results).enumerate() {
+        let mut temp = *a;
+        for (k, bit) in b.into_iter().enumerate() {
             // Calculate the shift (intermediate value << 2^k)
             let num_shift_bits = 1 << k;
-            let one_minus_bit = parser.sub(one, bit);
+
+            let res = if k == m - 1 {
+                *result
+            } else {
+                self.alloc_array::<BitRegister>(n)
+            };
 
             // For i< NUM_BITS - num_shift_bits, we have shifted_res[i] = temp[i + num_shift_bits]
-            for i in 0..(NUM_BITS - num_shift_bits) {
-                let shifted_res = temp[i + num_shift_bits];
-                let shifted_res_times_bit = parser.mul(bit, shifted_res);
-                let temp_times_one_minus_bit = parser.mul(one_minus_bit, temp[i]);
-                let expected_result = parser.add(shifted_res_times_bit, temp_times_one_minus_bit);
-                parser.assert_eq(expected_result, result[i]);
+            for i in 0..(n - num_shift_bits) {
+                self.set_select(
+                    &bit,
+                    &temp.get(i + num_shift_bits),
+                    &temp.get(i),
+                    &res.get(i),
+                );
             }
 
             // For i >= NUM_BITS - num_shift_bits, we have shifted_res[i] = 0
-            for i in (NUM_BITS - num_shift_bits)..NUM_BITS {
-                let temp_times_one_minus_bit = parser.mul(one_minus_bit, temp[i]);
-                let expected_result = parser.add(bit, temp_times_one_minus_bit);
-                parser.assert_eq(expected_result, result[i]);
+            for i in (n - num_shift_bits)..n {
+                self.set_select(&bit, &bit, &temp.get(i), &res.get(i));
             }
-
-            temp = result;
-        }
-    }
-}
-
-impl<F: Field, const NUM_BITS: usize, const LOG_N: usize> Instruction<F> for Shr<NUM_BITS, LOG_N> {
-    fn inputs(&self) -> Vec<MemorySlice> {
-        vec![*self.a.register(), *self.b.register()]
-    }
-
-    fn trace_layout(&self) -> Vec<MemorySlice> {
-        self.results.map(|x| *x.register()).to_vec()
-    }
-
-    fn constraint_degree(&self) -> usize {
-        2
-    }
-
-    fn write(&self, writer: &TraceWriter<F>, row_index: usize) {
-        let a = writer.read_array::<_, NUM_BITS>(&self.a, row_index);
-        let b = writer.read_array::<_, LOG_N>(&self.b, row_index);
-
-        let mut temp = a;
-        for (k, &bit) in b.iter().enumerate() {
-            let num_shift_bits = 1 << k;
-            let mut result = [F::ZERO; NUM_BITS];
-            for i in 0..(NUM_BITS - num_shift_bits) {
-                result[i] = bit * temp[i + num_shift_bits] + (F::ONE - bit) * temp[i];
-            }
-            for i in (NUM_BITS - num_shift_bits)..NUM_BITS {
-                result[i] = bit + (F::ONE - bit) * temp[i];
-            }
-            writer.write_array(&self.results[k], &result, row_index);
-            temp = result;
+            temp = res;
         }
     }
 }
 
 #[cfg(test)]
 pub mod tests {
+
     use rand::{thread_rng, Rng};
 
     use super::*;
@@ -109,31 +79,15 @@ pub mod tests {
     use crate::chip::AirParameters;
 
     #[derive(Debug, Clone)]
-    pub struct ShrTest<const N: usize, const LOG_N: usize>;
+    pub struct ShrTest<const N: usize, const M: usize>;
 
-    impl<const N: usize, const LOG_N: usize> const AirParameters for ShrTest<N, LOG_N> {
-        type Field = GoldilocksField;
-        type CubicParams = GoldilocksCubicParameters;
-
-        type Instruction = Shr<N, LOG_N>;
-
-        const NUM_FREE_COLUMNS: usize = 2 * N + LOG_N * N + N;
-
-        fn num_rows_bits() -> usize {
-            9
-        }
-    }
-
-    #[derive(Debug, Clone)]
-    pub struct ShrTestBuilder<const N: usize, const LOG_N: usize>;
-
-    impl<const N: usize, const LOG_N: usize> const AirParameters for ShrTestBuilder<N, LOG_N> {
+    impl<const N: usize, const M: usize> const AirParameters for ShrTest<N, M> {
         type Field = GoldilocksField;
         type CubicParams = GoldilocksCubicParameters;
 
         type Instruction = SelectInstruction<BitRegister>;
 
-        const NUM_FREE_COLUMNS: usize = 2 * N + LOG_N * N + N;
+        const NUM_FREE_COLUMNS: usize = 2 * N + M * N + N;
 
         fn num_rows_bits() -> usize {
             9
@@ -141,7 +95,7 @@ pub mod tests {
     }
 
     #[test]
-    fn test_shr_instruction() {
+    fn test_shr() {
         type F = GoldilocksField;
         type L = ShrTest<N, LOG_N>;
         const LOG_N: usize = 3;
@@ -152,101 +106,10 @@ pub mod tests {
 
         let a = builder.alloc_array::<BitRegister>(N);
         let b = builder.alloc_array::<BitRegister>(LOG_N);
-        let results: [_; LOG_N] = core::array::from_fn(|_| builder.alloc_array::<BitRegister>(N));
+        let result = builder.shr(&a, &b);
         let expected = builder.alloc_array::<BitRegister>(N);
 
-        let shr = Shr { a, b, results };
-        builder.register_instruction(shr);
-
-        builder.assert_expressions_equal(results[LOG_N - 1].expr(), expected.expr());
-
-        let air = builder.build();
-
-        let generator = ArithmeticGenerator::<L>::new(&air);
-        let writer = generator.new_writer();
-
-        let mut rng = thread_rng();
-
-        let to_bits_le = |x: u8| {
-            let mut bits = [0u8; 8];
-            for i in 0..8 {
-                bits[i] = (x >> i) & 1;
-            }
-            bits
-        };
-
-        let to_val = |bits: &[u8]| bits.iter().enumerate().map(|(i, b)| b << i).sum::<u8>();
-        for i in 0..L::num_rows() {
-            let a_val = rng.gen::<u8>();
-            let a_bits = to_bits_le(a_val);
-            let b_bits = [0, 0, 0];
-            let b_val = to_val(&b_bits);
-            assert_eq!(a_val, to_val(&a_bits));
-            let expected_val = a_val >> b_val;
-            let expected_bits = to_bits_le(expected_val);
-            writer.write_array(&a, a_bits.map(|a| F::from_canonical_u8(a)), i);
-            writer.write_array(&b, b_bits.map(|b| F::from_canonical_u8(b)), i);
-            writer.write_array(&expected, expected_bits.map(|b| F::from_canonical_u8(b)), i);
-            writer.write_instruction(&shr, i);
-        }
-
-        let trace = generator.trace_clone();
-
-        for window in trace.windows_iter() {
-            let mut window_parser = TraceWindowParser::new(window, &[], &[], &[]);
-            air.eval(&mut window_parser);
-        }
-
-        let stark = Starky::<_, { L::num_columns() }>::new(air);
-        let config = SC::standard_fast_config(L::num_rows());
-
-        // Generate proof and verify as a stark
-        test_starky(&stark, &config, &generator, &[]);
-
-        // Test the recursive proof.
-        test_recursive_starky(stark, config, generator, &[]);
-    }
-
-    #[test]
-    fn test_shr_builder_methods() {
-        type F = GoldilocksField;
-        type L = ShrTestBuilder<N, LOG_N>;
-        const LOG_N: usize = 3;
-        const N: usize = 8;
-        type SC = PoseidonGoldilocksStarkConfig;
-
-        let mut builder = AirBuilder::<L>::new();
-
-        let a = builder.alloc_array::<BitRegister>(N);
-        let b = builder.alloc_array::<BitRegister>(LOG_N);
-        let results: [_; LOG_N] = core::array::from_fn(|_| builder.alloc_array::<BitRegister>(N));
-        let expected = builder.alloc_array::<BitRegister>(N);
-
-        let mut results = vec![];
-
-        let mut temp = a;
-        for (k, bit) in b.into_iter().enumerate() {
-            // Calculate the shift (intermediate value << 2^k)
-            let num_shift_bits = 1 << k;
-
-            let mut res = Vec::new();
-            // For i< NUM_BITS - num_shift_bits, we have shifted_res[i] = temp[i + num_shift_bits]
-            for i in 0..(N - num_shift_bits) {
-                let result_i = builder.select(&bit, &temp.get(i + num_shift_bits), &temp.get(i));
-                res.push(result_i);
-            }
-
-            // For i >= NUM_BITS - num_shift_bits, we have shifted_res[i] = 0
-            for i in (N - num_shift_bits)..N {
-                let result_i = builder.select(&bit, &bit, &temp.get(i));
-                res.push(result_i);
-            }
-            let idx = res[0].register().index();
-            let value = ArrayRegister::from_register_unsafe(MemorySlice::Local(idx, N));
-            results.push(value);
-            temp = results[k];
-        }
-        builder.assert_expressions_equal(results[LOG_N - 1].expr(), expected.expr());
+        builder.assert_expressions_equal(result.expr(), expected.expr());
 
         let air = builder.build();
 
