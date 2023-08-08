8b73cb99bdf42a32d0d6988b7b668b6483546d24
Tamir Hemo <tamir@succinct.xyz>
2023-08-07 17:41:40 -0700

    shr with shift < 8

--- curta/src/chip/uint/operations/shr.rs
+++ curta/src/chip/uint/operations/shr.rs
@@ -1,10 +1,12 @@
 use crate::chip::builder::AirBuilder;
-use crate::chip::constraint::arithmetic::expression::ArithmeticExpression;
+use crate::chip::register::Register;
 use crate::chip::uint::bytes::lookup_table::builder_operations::ByteLookupOperations;
 use crate::chip::uint::bytes::operations::instruction::ByteOperationInstruction;
 use crate::chip::uint::bytes::operations::value::ByteOperation;
+use crate::chip::uint::bytes::register::ByteRegister;
 use crate::chip::uint::register::ByteArrayRegister;
 use crate::chip::AirParameters;
+use crate::math::prelude::*;
 
 impl<L: AirParameters> AirBuilder<L> {
     pub fn set_bit_shr<const N: usize>(
@@ -16,5 +18,32 @@ impl<L: AirParameters> AirBuilder<L> {
     ) where
         L::Instruction: From<ByteOperationInstruction>,
     {
+        let a_bytes = a.bytes();
+        let result_bytes = result.bytes();
+
+        let shift = shift % (N * 8);
+        let byte_shift = shift / 8;
+        let bit_shift = shift % 8;
+
+        let shifts_results: [_; N] = core::array::from_fn(|_| self.alloc::<ByteRegister>());
+        let carries: [_; N] = core::array::from_fn(|_| self.alloc::<ByteRegister>());
+
+        for i in (N - byte_shift)..N {
+            self.assert_zero(&result_bytes.get(i));
+        }
+
+        for ((res, carry), byte) in shifts_results.into_iter().zip(carries).zip(a_bytes) {
+            let shr_carry = ByteOperation::ShrCarry(byte, shift as u8, res, carry);
+            self.set_byte_operation(&shr_carry, operations);
+        }
+
+        assert!(bit_shift < 8, "Shifts larger than 8 are not supported yet");
+        let mult = L::Field::from_canonical_u32(1 << (8 - bit_shift));
+        for (i, res_byte) in result_bytes.iter().enumerate().take(N - 1) {
+            let expected_res = shifts_results[i].expr() + carries[i + 1].expr() * mult;
+            self.set_to_expression(&res_byte, expected_res);
+        }
+        
+        self.set_to_expression(&result_bytes.get(N-1), shifts_results[N-1].expr());
     }
 }
