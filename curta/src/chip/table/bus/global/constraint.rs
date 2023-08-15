d5fe893a97e0049f4d8766baf5af8bfa3bc22404
Tamir Hemo <tamir@succinct.xyz>
2023-08-14 17:37:07 -0700

    global bus values

--- curta/src/chip/table/bus/global/constraint.rs
+++ curta/src/chip/table/bus/global/constraint.rs
@@ -7,6 +7,15 @@ use crate::math::extension::cubic::parameters::CubicParameters;
 impl<E: CubicParameters<AP::Field>, AP: CubicParser<E>> AirConstraint<AP> for Bus<E> {
     fn eval(&self, parser: &mut AP) {
         let mut acc_prod = parser.one_extension();
+
+        // Accumulate global inputs
+        let beta = self.challenge.eval(parser);
+        for register in self.global_inputs.iter() {
+            let value = register.eval(parser);
+            let beta_minus_value = parser.sub_extension(beta, value);
+            acc_prod = parser.mul_extension(acc_prod, beta_minus_value);
+        }
+
         for channel in self.channels.iter() {
             let value = channel.eval(parser);
             acc_prod = parser.mul_extension(acc_prod, value);
--- curta/src/chip/table/bus/global/mod.rs
+++ curta/src/chip/table/bus/global/mod.rs
@@ -13,6 +13,8 @@ use core::marker::PhantomData;
 use super::channel::BusChannel;
 use crate::chip::builder::AirBuilder;
 use crate::chip::register::cubic::CubicRegister;
+use crate::chip::register::memory::MemorySlice;
+use crate::chip::register::RegisterSerializable;
 use crate::chip::AirParameters;
 
 /// The main bus enforcing the constraint
@@ -22,6 +24,8 @@ use crate::chip::AirParameters;
 pub struct Bus<E> {
     /// The channels of the bus
     channels: Vec<CubicRegister>,
+    // Public inputs to the bus
+    global_inputs: Vec<CubicRegister>,
     // The challenge used
     challenge: CubicRegister,
     _marker: PhantomData<E>,
@@ -32,6 +36,7 @@ impl<L: AirParameters> AirBuilder<L> {
         let challenge = self.alloc_challenge::<CubicRegister>();
         Bus {
             channels: Vec::new(),
+            global_inputs: Vec::new(),
             challenge,
             _marker: PhantomData,
         }
@@ -55,6 +60,18 @@ impl<E: Clone> Bus<E> {
         builder.bus_channels.push(channel);
         index
     }
+
+    pub fn insert_global_value(&mut self, register: &CubicRegister) {
+        match register.register() {
+            MemorySlice::Global(..) => {
+                self.global_inputs.push(*register);
+            }
+            MemorySlice::Public(..) => {
+                self.global_inputs.push(*register);
+            }
+            _ => panic!("Expected public or global register"),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -63,8 +80,11 @@ mod tests {
 
     use super::*;
     use crate::chip::builder::tests::*;
+    use crate::chip::register::bit::BitRegister;
+    use crate::chip::register::Register;
     use crate::chip::AirParameters;
     use crate::math::extension::cubic::element::CubicElement;
+    use crate::math::prelude::*;
 
     #[derive(Debug, Clone)]
     struct BusTest;
@@ -73,30 +93,40 @@ mod tests {
         type Field = GoldilocksField;
         type CubicParams = GoldilocksCubicParameters;
 
-        const NUM_FREE_COLUMNS: usize = 6;
+        const NUM_FREE_COLUMNS: usize = 7;
         const EXTENDED_COLUMNS: usize = 12;
 
         type Instruction = EmptyInstruction<GoldilocksField>;
 
         fn num_rows_bits() -> usize {
-            10
+            16
         }
     }
 
     #[test]
     fn test_bus() {
         type L = BusTest;
+        type F = GoldilocksField;
         type SC = PoseidonGoldilocksStarkConfig;
+
+        let num_public_vals = 1000;
 
         let mut builder = AirBuilder::<L>::new();
         let x_1 = builder.alloc::<CubicRegister>();
         let x_2 = builder.alloc::<CubicRegister>();
 
+        let bit = builder.alloc::<BitRegister>();
+
         let mut bus = builder.new_bus();
         let channel_idx = bus.new_channel(&mut builder);
 
-        builder.input_to_bus(channel_idx, x_1);
+        builder.input_to_bus_filtered(channel_idx, x_1, bit.expr());
         builder.output_from_bus(channel_idx, x_2);
+
+        let x_pub = builder.alloc_array_public::<CubicRegister>(num_public_vals);
+        for x in x_pub.iter() {
+            bus.insert_global_value(&x);
+        }
 
         builder.constrain_bus(bus);
 
@@ -104,20 +134,30 @@ mod tests {
 
         let generator = ArithmeticGenerator::<L>::new(&air);
         let writer = generator.new_writer();
-        for i in 0..L::num_rows() {
+
+        for (i, x) in x_pub.iter().enumerate() {
+            let x_pub_val = CubicElement([GoldilocksField::rand(); 3]);
+            writer.write(&x, &x_pub_val, 0);
+            writer.write(&x_2, &x_pub_val, L::num_rows() - i - 1);
+            writer.write(&bit, &F::ZERO, i);
+        }
+        for i in num_public_vals..L::num_rows() {
             let a = CubicElement([GoldilocksField::rand(); 3]);
-            writer.write(&x_1, &a, L::num_rows() - 1 - i);
-            writer.write(&x_2, &a, i);
+            writer.write(&bit, &F::ONE, i);
+            writer.write(&x_1, &a, i);
+            writer.write(&x_2, &a, L::num_rows() - i - 1);
         }
 
         let stark = Starky::from_chip(air);
 
         let config = SC::standard_fast_config(L::num_rows());
 
+        let public_inputs = writer.0.public.read().unwrap().clone();
+
         // Generate proof and verify as a stark
-        test_starky(&stark, &config, &generator, &[]);
+        test_starky(&stark, &config, &generator, &public_inputs);
 
         // Test the recursive proof.
-        test_recursive_starky(stark, config, generator, &[]);
+        test_recursive_starky(stark, config, generator, &public_inputs);
     }
 }
