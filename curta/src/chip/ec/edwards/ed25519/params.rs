aea75fe417f52fae7aa6be00ed663b712d70361a
Kevin Jue <kjue235@gmail.com>
2023-10-31 15:09:25 -0700

    feat: ed25519 scalar field parameters (#117)

--- curta/src/chip/ec/edwards/ed25519/params.rs
+++ curta/src/chip/ec/edwards/ed25519/params.rs
@@ -28,6 +28,24 @@ impl FieldParameters for Ed25519BaseField {
     }
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
+pub struct Ed25519ScalarField;
+
+impl FieldParameters for Ed25519ScalarField {
+    const NB_BITS_PER_LIMB: usize = 16;
+    const NB_LIMBS: usize = 16;
+    const NB_WITNESS_LIMBS: usize = 2 * Self::NB_LIMBS - 2;
+    const MODULUS: [u16; MAX_NB_LIMBS] = [
+        54253, 23797, 25370, 22546, 40150, 41719, 63966, 5342, 0, 0, 0, 0, 0, 0, 0, 4096, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    ];
+    const WITNESS_OFFSET: usize = 1usize << 20;
+
+    fn modulus() -> BigUint {
+        (BigUint::one() << 252) + BigUint::from(27742317777372353535851937790883648493u128)
+    }
+}
+
 impl EllipticCurveParameters for Ed25519Parameters {
     type BaseField = Ed25519BaseField;
 }
